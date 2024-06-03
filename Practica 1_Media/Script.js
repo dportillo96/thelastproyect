fed6a743ed64925ea759e3ad7f81ecec3e0ec90e
dportillo96 <daniportillo@correo.ugr.es>
2024-06-03 22:41:13 +0200

    Add files via upload

---
+++ Practica 1_Media/Script.js
@@ -0,0 +1,50 @@
+function Frame__1_0(ID){Pause();
+
+}
+
+function Frame__2_0(ID){Pause();
+
+
+}
+
+function Frame__4_0(ID){Pause();
+
+
+}
+
+function Frame__5_0(ID){Pause();
+
+
+}
+
+function Frame__6_0(ID){Pause();
+
+
+}
+
+function Frame__7_0(ID){Pause();
+
+
+}
+
+function Button1_OnClick(){GotoSceneName("Galeria");
+}
+
+function Button4_OnClick(){GotoSceneNameAndPlay("Galeria2", 0);
+}
+
+function Button5_OnClick(){GotoSceneNameAndPlay("Galeria3", 0);
+}
+
+function Button6_OnClick(){GotoSceneNameAndPlay("Galeria4", 0);
+}
+
+function Button7_OnClick(){GotoSceneNameAndPlay("Galeria5", 0);
+}
+
+function Button8_OnClick(){GotoSceneNameAndPlay("Galeria", 0);
+}
+
+function menu_OnClick(){GotoSceneNameAndPlay("Menu",0);
+}
+
